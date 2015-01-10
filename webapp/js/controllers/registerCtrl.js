af0f3a04577da9d11d38c8f69bb67c0b8cd28f07
DefeNder93 <alexdefender93@gmail.com>
2015-01-10 13:37:12 +0200

    регистрация

--- webapp/js/controllers/registerCtrl.js
+++ webapp/js/controllers/registerCtrl.js
@@ -3,36 +3,14 @@
  * @class
  */
 angular.module('reChat.registerCtrl', [])
-    .controller('RegisterCtrl', ['$scope','ipCookie','JsonRpsService','$location','$timeout','$route',
-        function($scope,ipCookie,JsonRpsService,$location,$timeout,$route) {
+    .controller('RegisterCtrl', ['$scope','JsonRpsService','ipCookie','$location','$timeout','$route',
+        function($scope, JsonRpsService, ipCookie, $location, $timeout, $route) {
             var JSONRPC_URL = '/ajax';
             var USERNAME_COOKIE = 'username';
-            /*sendJSONRPC(JSONRPC_URL, "register", {username: $('#register-login').val(), password: $('#register-password').val() }, function (response) { //$('#register-secret').val()
-                if (response.status == "error") {
-                    $("#register-error").text(response.message);
-                }
-                else {
-                    $("#register-error").text("successfully registered")
-                    var isRegistered = 1;
-                    sendJSONRPC(JSONRPC_URL, "login", {username: $('#register-login').val(), password: $('#register-password').val() }, function (response) {
-                        if (response.status == "error") {
-                            $("#register-error").text(response.message);
-                        }
-                        else {
-                            var next = getURLParameter('next');
-                            if (!next) {
-                                next = "/"
-                            }
-                            window.location = next
-                        }
-                    });
-                    return false
-                }
-            });*/
 
             $scope.register = function() {
                 console.log("Try register");
-                JsonRpsServce.sendJSONRPC(JSONRPC_URL, "register",{username: $scope.userName, password: $scope.userPassword }, function(response) {
+                JsonRpsService.sendJSONRPC(JSONRPC_URL, "register",{username: $scope.userName, password: $scope.userPassword }, function(response) {
                     console.log("success register");
                     JsonRpsService.sendJSONRPC(JSONRPC_URL, "login", {username: $scope.userName, password: $scope.userPassword }, function(response) {
                         console.log("Success - " + response.message);
